dcb57f932ea8437587a65d921f52f11e5a90b0d6
Wade Barnes <wade@neoterictech.ca>
2023-04-12 07:41:47 -0700

    Upgrade the backup container.
    - Upgrade to v2.6.0.
    - Add support for multi-instance deployment.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/templates/aries-mediator-backup/aries-mediator-backup-deploy.overrides.sh
+++ openshift/templates/aries-mediator-backup/aries-mediator-backup-deploy.overrides.sh
@@ -11,19 +11,19 @@ fi
 # ------------------------------------------------------------------------
 # The generated config map is used to update the Backup configuration.
 # ========================================================================
-CONFIG_MAP_NAME=backup-conf
+CONFIG_MAP_NAME=${CONFIG_MAP_NAME:-backup-conf}
 SOURCE_FILE=$( dirname "$0" )/config/backup.conf
 
 OUTPUT_FORMAT=json
 OUTPUT_FILE=${CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
 
 printStatusMsg "Generating ConfigMap; ${CONFIG_MAP_NAME} ..."
-generateConfigMap "${CONFIG_MAP_NAME}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
+generateConfigMap "${CONFIG_MAP_NAME}${SUFFIX}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
 
 
 if createOperation; then
   # Get the webhook URL
-  readParameter "WEBHOOK_URL - Please provide the webhook endpoint URL.  If left blank, the webhook integration feature will be disabled:" WEBHOOK_URL "false" 
+  readParameter "WEBHOOK_URL - Please provide the webhook endpoint URL.  If left blank, the webhook integration feature will be disabled:" WEBHOOK_URL "false"
 else
   printStatusMsg "Update operation detected ...\nSkipping the prompts for the WEBHOOK_URL secret ...\n"
   writeParameter "WEBHOOK_URL" "prompt_skipped" "false"
