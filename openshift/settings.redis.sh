be0b9cee437ee4290c9028db7bb9e502c9774d27
Wade Barnes <wade@neoterictech.ca>
2023-05-19 12:23:25 -0700

    Add shared Redis cluster
    - Configurations copied from here; https://github.com/bcgov/trust-over-ip-configurations
    - Upgrade to Redis v7.x.x
    - Added initialization script as a note in the template
    - Adjusted Redis node resources to minimize throttling
    - Fix issue with update-ip script for redis cluster
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

---
+++ openshift/settings.redis.sh
@@ -0,0 +1,4 @@
+# Description: Shared Redis Cluster Profile - A shared persistent queus for agents.
+export SKIP_PIPELINE_PROCESSING=1
+export include_templates="redis-cluster-deploy"
+export ignore_templates=""
