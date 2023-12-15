40b644d9d3b7970d70cc2eae2152c210cf41aedf
Wade Barnes <wade@neoterictech.ca>
2023-12-15 07:41:23 -0800

    Migrate existing mediator instances to a persistent queue configuration
    - This allows us to reuse the existing invites and URLs, eliminating the need to migrate the BC Wallet to new mediator instances.
      - It also eliminates any disruption in service for existing users.
    - `-pq` instances will be scaled down and later decommissioned.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/settings.pq.sh
+++
@@ -1,4 +0,0 @@
-# Description: A mediator instance with a persistent queue.
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="aries-mediator-agent-build aries-mediator-agent-deploy aries-mediator-db-deploy aries-mediator-proxy-deploy "
-export ignore_templates=""
