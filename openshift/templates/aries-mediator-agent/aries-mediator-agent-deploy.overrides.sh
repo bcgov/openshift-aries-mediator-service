111a4bc5de4c36afd5161d8e9f5c399927e16d17
Wade Barnes <wade@neoterictech.ca>
2023-12-19 09:46:05 -0800

    Add Firebase plugin and configuration
    - Had to switch to using a discrete Dockerfile so we could better control the list of plugins installed.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/templates/aries-mediator-agent/aries-mediator-agent-deploy.overrides.sh
+++ openshift/templates/aries-mediator-agent/aries-mediator-agent-deploy.overrides.sh
@@ -29,12 +29,22 @@ if createOperation; then
   readParameter "WALLET_SEED - Please provide the indy wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" WALLET_SEED $(generateSeed) "false"
   readParameter "WALLET_KEY - Please provide the wallet encryption key for the environment.  If left blank, a 48 character long base64 encoded value will be randomly generated using openssl:" WALLET_KEY $(generateKey) "false"
   readParameter "ADMIN_API_KEY - Please provide the key for the agent's Admin API.  If left blank, a 32 character long base64 encoded value will be randomly generated using openssl:" ADMIN_API_KEY $(generateKey 32) "false"
+
+  readParameter "FIREBASE_PROJECT_ID - Please provide the firebase project id for the environment.  The defaut is a blank string:" FIREBASE_PROJECT_ID "" "false"
+  readParameter "FIREBASE_NOTIFICATION_TITLE - Please provide the firebase notification title for the environment.  The defaut is a blank string:" FIREBASE_NOTIFICATION_TITLE "" "false"
+  readParameter "FIREBASE_NOTIFICATION_BODY - Please provide the firebase notification body for the environment.  The defaut is a blank string:" FIREBASE_NOTIFICATION_BODY "" "false"
+  readParameter "FIREBASE_SERVICE_ACCOUNT - Please provide the firebase service account infomration, in the form of flattened account json, for the environment.  The defaut is a blank string:" FIREBASE_SERVICE_ACCOUNT "" "false"
 else
   # Secrets are removed from the configurations during update operations ...
-  printStatusMsg "Update operation detected ...\nSkipping the prompts for WALLET_SEED secret... \n"
+  printStatusMsg "Update operation detected ...\nSkipping the prompts for the WALLET_SEED, WALLET_KEY, ADMIN_API_KEY, FIREBASE_PROJECT_ID, FIREBASE_NOTIFICATION_TITLE, FIREBASE_NOTIFICATION_BODY, and FIREBASE_SERVICE_ACCOUNT secrets... \n"
   writeParameter "WALLET_SEED" "prompt_skipped" "false"
   writeParameter "WALLET_KEY" "prompt_skipped" "false"
   writeParameter "ADMIN_API_KEY" "prompt_skipped" "false"
+
+  writeParameter "FIREBASE_PROJECT_ID" "prompt_skipped" "false"
+  writeParameter "FIREBASE_NOTIFICATION_TITLE" "prompt_skipped" "false"
+  writeParameter "FIREBASE_NOTIFICATION_BODY" "prompt_skipped" "false"
+  writeParameter "FIREBASE_SERVICE_ACCOUNT" "prompt_skipped" "false"
 fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
